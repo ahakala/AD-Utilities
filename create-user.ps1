ec1a254e792dd4a3fc35984a436d0947439f9677
Andrew Hakala <Hakala@TS-OSXHakalaLT.local>
2016-05-26 14:08:32 -0500

    initial commit

---
+++ create-user.ps1
@@ -0,0 +1,1 @@
+import-csv .\demo-users.csv | % {New-ADUser -GivenName $_.GivenName -Surname $_.Surname -Name $_.Name -SamAccountName $_.SamAccountName -Description $_.Description -Department $_.Department -EmployeeID $_.EmployeeID -Path $_.Path -Enabled $True -AccountPassword (ConvertTo-SecureString $_.Password -AsPlainText -force) -PasswordNeverExpires $True}
---
+++ readme.md
@@ -0,0 +1,6 @@
+# AD Quick Setup
+### Overview
+This script and CSV is designed to quickly populate users in a sample AD environment. This could be used to populate a production environment as well, extra values and secure passwords should be used for this purpose.
+
+### Notes
+Change the Path Value to reflect the path where the Users will be located. 
